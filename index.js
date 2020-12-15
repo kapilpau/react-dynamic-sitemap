48678cce40b86e2acd8a4ad33771b3b2fe33c2a6
Kapil Pau <kapilpau@hotmail.com>
2020-12-15 23:20:37 +0000

    Add slug support

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 exports["default"] = Sitemap;
 
@@ -14,48 +14,46 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 var builder = require("xmlbuilder");
 
 function Sitemap(props) {
-	var host, protocol = "";
-	var renderType = "server";
-	if (props.hostname)
-		host = props.hostname;
-	if (props.protocol)
-		protocol = props.protocol;
-	if (typeof window !== "undefined" && window) {
-		renderType = "client";
-		host = window.location.hostname;
-		protocol = window.location.protocol;
-	}
-	var hostname = protocol + "//" + host;
-	var Routes = props.routes;
+  var hostname = window.location.protocol + "//" + window.location.hostname;
+  var Routes = props.routes;
 
-	var sitemap = function sitemap() {
-		var paths = Routes(props).props.children;
-		var xml = builder.create("urlset", {
-			encoding: "utf-8"
-		}).att("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
-		paths.forEach(function (path) {
-			if (path.props.sitemapIndex) {
-				var item = xml.ele("url");
-				item.ele("loc", hostname + path.props.path);
-				item.ele("priority", path.props.priority || 0);
-				item.ele("changefreq", path.props.changefreq || "never");
-			}
-		});
-		return xml.end({
-			pretty: props.prettify
-		});
-	};
-	if (renderType === "client")
-		return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement("div", {
-			style: {
-				whiteSpace: "pre-wrap"
-			}
-		}, sitemap()));
-	else
-		return sitemap();
+  var sitemap = function sitemap() {
+    var paths = Routes(props).props.children;
+    var xml = builder.create("urlset", {
+      encoding: "utf-8"
+    }).att("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
+    paths.forEach(function (path) {
+      var slugs = path.props.slugs || [{}];
+      slugs.forEach(function (slug) {
+        var uri = path.props.path;
+        Object.keys(slug).forEach(function (key) {
+          var value = slug[key];
+          var midStringRegex = new RegExp("/:".concat(key, "/"), "g");
+          var endStringRegex = new RegExp("/:".concat(key, "$"));
+          if (uri.match(midStringRegex)) uri = uri.replace(midStringRegex, "/".concat(value, "/"));else uri = uri.replace(endStringRegex, "/".concat(value));
+        });
+
+        if (path.props.sitemapIndex) {
+          var item = xml.ele("url");
+          item.ele("loc", hostname + uri);
+          item.ele("priority", path.props.priority || 0);
+          item.ele("changefreq", path.props.changefreq || "never");
+        }
+      });
+    });
+    return xml.end({
+      pretty: props.prettify
+    });
+  };
+
+  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement("div", {
+    style: {
+      whiteSpace: "pre-wrap"
+    }
+  }, sitemap()));
 }
 
 Sitemap.propTypes = {
-	prettify: _propTypes["default"].bool,
-	routes: _propTypes["default"].function
+  prettify: _propTypes["default"].bool,
+  routes: _propTypes["default"].object
 };
--- src/index.js
+++ src/index.js
@@ -10,13 +10,26 @@ export default function Sitemap(props) {
 	const sitemap = () => {
 		let paths = Routes(props).props.children;
 		let xml = builder.create("urlset", {encoding: "utf-8"}).att("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
-		paths.forEach(path => {
-			if (path.props.sitemapIndex) {
-				let item = xml.ele("url");
-				item.ele("loc", hostname + path.props.path);
-				item.ele("priority", path.props.priority || 0);
-				item.ele("changefreq", path.props.changefreq || "never");
-			}
+		paths.forEach(function (path) {
+			const slugs = path.props.slugs || [{}];
+			slugs.forEach(slug => {
+				let uri = path.props.path;
+				Object.keys(slug).forEach(key => {
+					const value = slug[key];
+					let midStringRegex = new RegExp(`/:${key}/`, "g");
+					let endStringRegex = new RegExp(`/:${key}$`);
+					if (uri.match(midStringRegex))
+						uri = uri.replace(midStringRegex, `/${value}/`);
+					else
+						uri = uri.replace(endStringRegex, `/${value}`);
+				});
+				if (path.props.sitemapIndex) {
+					var item = xml.ele("url");
+					item.ele("loc", hostname + uri);
+					item.ele("priority", path.props.priority || 0);
+					item.ele("changefreq", path.props.changefreq || "never");
+				}
+			});
 		});
 		return xml.end({ pretty: props.prettify });
 	};
